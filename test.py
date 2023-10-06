66d20e511bca21538a6971633504601f4aa871cd
Guadalupe Bernal <guadabernal@gmail.com>
2023-10-06 14:00:29 -0400

    test

--- test.py
+++ test.py
@@ -26,8 +26,8 @@ framerate = 30
 camera_timestamp = False
 # -----------------------------------------------------------------------------------------------
 # Motion sensitivity
-motion_vectors_norm = 90    # mvecs norm
-motion_density = 90         # number of pixels with |mvecs| > motion_density
+motion_vectors_norm = 95    # mvecs norm
+motion_density = 95         # number of pixels with |mvecs| > motion_density
 motion_min_log_time = 1     # seconds
 # -----------------------------------------------------------------------------------------------
 
