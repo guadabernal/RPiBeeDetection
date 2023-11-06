075ecaad8ea8f641f97e3fec8faf7c1c5d92fb97
Guadalupe Bernal <guadabernal@gmail.com>
2023-11-06 16:37:09 -0500

    update

--- AudioRecordingTest.py
+++ AudioRecordingTest.py
@@ -15,8 +15,8 @@ for i in range(0, numdevices):
   if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
     name = p.get_device_info_by_host_api_device_index(0, i).get('name')
     print("Input Device id ", i, " - ", name)
-  if (name == "ac108"):
-    print(i)
+    if (name == "ac108"):
+      print(i)
   
 RESPEAKER_INDEX = 10  # refer to input device id
 CHUNK = 1024
