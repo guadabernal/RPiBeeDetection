50528f5bf9ba8667d40dd5025aab75fc621c564f
Guadalupe Bernal <guadabernal@gmail.com>
2023-11-06 16:34:06 -0500

    update

--- AudioRecordingTest.py
+++ AudioRecordingTest.py
@@ -10,9 +10,9 @@ info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 
 # run getDeviceInfo.py to get index
-for i in range(0, numdevices):
-  if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-    print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+# for i in range(0, numdevices):
+#   if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+    # print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
   
 exit()
 
